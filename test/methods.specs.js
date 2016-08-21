5ac891b744af50da50c063cdc645a54f6111971c
Vladimir Shushkov <vladimir@shushkov.ru>
2016-08-21 18:06:23 +1000

    removed excess test files (methods.specs and reducer.specs)

--- test/methods.specs.js
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai';
-import { combineReducers } from 'redux';
-import { createModel } from '../src';
-import { fetch } from './utils';
-import api from '../src/helpers/api';
-
-describe.skip('Methods', () => {
-
-});
--- test/reducer.specs.js
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import api from '../src/helpers/api';
-import { createModel } from '../src';
-import { defaultReducer } from '../src/reducer';
-
-describe.skip('Reducers', () => {
-
-});
